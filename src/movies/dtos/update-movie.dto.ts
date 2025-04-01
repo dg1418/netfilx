ec1f6b3c73db3603bd4d27834538aab3ea2c2aa8
dg1418 <dg1418@naver.com>
2025-04-01 16:58:31 +0900

    10강 mapped types 중 partialType 적용해보기

--- src/movies/dtos/update-movie.dto.ts
+++ src/movies/dtos/update-movie.dto.ts
@@ -1,34 +1,4 @@
-import {
-  ArrayNotEmpty,
-  IsArray,
-  IsInt,
-  IsNotEmpty,
-  IsOptional,
-  IsPositive,
-  IsString,
-} from 'class-validator';
+import { CreateMovieDto } from './create-movie.dto';
+import { PartialType } from '@nestjs/mapped-types';
 
-export class UpdateMovieDto {
-  @IsNotEmpty()
-  @IsString()
-  @IsOptional()
-  title?: string;
-
-  @IsNotEmpty()
-  @IsString()
-  @IsOptional()
-  detail?: string;
-
-  @IsArray()
-  @ArrayNotEmpty()
-  @IsInt({ each: true })
-  @IsPositive({ each: true })
-  @IsOptional()
-  genreIds?: number[];
-
-  @IsNotEmpty()
-  @IsInt()
-  @IsPositive()
-  @IsOptional()
-  directorId?: number;
-}
+export class UpdateMovieDto extends PartialType(CreateMovieDto) {}
