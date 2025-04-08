e72ef94c7e3d4204728e0e7e1d8c06e321f4b46e
dg1418 <dg1418@naver.com>
2025-04-08 19:22:19 +0900

    12강 환경변수 const 정리

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,7 @@ import { Genre } from './genre/entity/genre.entity';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { User } from './user/entity/user.entity';
+import { envVariableKeys } from './common/const/env.const';
 
 @Module({
   imports: [
@@ -33,12 +34,12 @@ import { User } from './user/entity/user.entity';
     TypeOrmModule.forRootAsync({
       // forRoot 가 아닌 forRootAsync를 하는 이유는 ConfigModule의 발리데이션이 끝나고 동기적으로 시작해야 하기 때문
       useFactory: (configService: ConfigService) => ({
-        type: configService.get<string>('DB_TYPE') as 'postgres', // config모듈이 아닌 기존 nodejs의 환경설정 방법
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_DATABASE'),
+        type: configService.get<string>(envVariableKeys.dbType) as 'postgres', // config모듈이 아닌 기존 nodejs의 환경설정 방법
+        host: configService.get<string>(envVariableKeys.dbHost),
+        port: configService.get<number>(envVariableKeys.dbPort),
+        username: configService.get<string>(envVariableKeys.dbUsername),
+        password: configService.get<string>(envVariableKeys.dbPassword),
+        database: configService.get<string>(envVariableKeys.dbDatabase),
         entities: [Movie, MovieDetail, Director, Genre, User], // typeorm 리포클래스들을 넣어서 설정하나봄
         synchronize: true, // entity를 보고 테이블을 구성하잖아? entitiy 변경되면 자동 마이그레인션 하는 옵션 ㄷㄷ
         // 개발환경에서만 true로 하고 실제 환경에서는 false로 하기
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,15 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Role, User } from 'src/user/entity/user.entity';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
-import { throwIfEmpty } from 'rxjs';
+import { envVariableKeys } from 'src/common/const/env.const';
 
 @Injectable()
 export class AuthService {
@@ -61,22 +65,28 @@ export class AuthService {
       throw new BadRequestException('토큰 포멧이 잘못되었습니다.');
     }
 
-    // payload를 가져오면서 검증도 함.
-    const payload = await this.jwtService.verifyAsync(token, {
-      secret: this.configService.get<string>('REFRESH_TOKEN_SECRET'), // 이부분 에러남 수정해야함
-    });
+    try {
+      // payload를 가져오면서 검증도 함.
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: this.configService.get<string>(
+          envVariableKeys.refreshTokenSecret,
+        ), // 이부분 에러남 수정해야함
+      });
 
-    if (isRefreshToken) {
-      if (payload.type !== 'refresh') {
-        throw new BadRequestException('Refresh 토큰을 입력해주세요!');
+      if (isRefreshToken) {
+        if (payload.type !== 'refresh') {
+          throw new BadRequestException('Refresh 토큰을 입력해주세요!');
+        }
+      } else {
+        if (payload.type !== 'access') {
+          throw new BadRequestException('Access 토큰을 입력해주세요!');
+        }
       }
-    } else {
-      if (payload.type !== 'access') {
-        throw new BadRequestException('Access 토큰을 입력해주세요!');
-      }
+
+      return payload;
+    } catch (e) {
+      throw new UnauthorizedException('Access 토큰이 만료 되었습니다.');
     }
-
-    return payload;
   }
 
   async register(rawToken: string) {
@@ -94,7 +104,7 @@ export class AuthService {
     // 3. 비번 해쉬를 위해 bcrypt 사용
     const hash = await bcrypt.hash(
       password,
-      this.configService.get<number>('HASH_ROUNDS'),
+      this.configService.get<number>(envVariableKeys.hashRounds),
     );
 
     await this.userRepository.save({
@@ -127,11 +137,11 @@ export class AuthService {
 
   async issueToken(user: { id: number; role: Role }, isRefreshToken: boolean) {
     const refreshTokenSecret = this.configService.get<string>(
-      'REFRESH_TOKEN_SECRET',
+      envVariableKeys.refreshTokenSecret,
     );
 
     const accessTokenSecret = this.configService.get<string>(
-      'ACCESS_TOKEN_SECRET',
+      envVariableKeys.accessTokenSecret,
     );
 
     return this.jwtService.signAsync(
