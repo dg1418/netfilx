6664dc2453ece0c5bb39dfcdfc151bac27320940
dg1418 <dg1418@naver.com>
2026-01-14 15:29:40 +0900

    28강 testing : userService findOne unit test 작성

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
+import { NotFoundException } from '@nestjs/common';
 
 // 7. mock데이터를 직접만들어줬음. 테스트용 객체이고, 메서드 이름만 똑같이한후 실제 함수는 jest.fn()으로 mock 함수를 넣어줌
 const mockUserRepository = {
@@ -68,7 +69,36 @@ describe('UserService', () => {
       expect(mockUserRepository.findAndCount).toHaveBeenCalled();
     });
   });
-  describe('findOne', () => {});
+
+  describe('findOne', () => {
+    it('should return user by id', async () => {
+      const user = {
+        id: 1,
+        email: 'test@codefactory.ai',
+      };
+
+      // mockUserRepository.findOne.mockResolvedValue(user);
+      // findAll 메서드 테스팅할때는 위 주석같은 세팅을 했는데 아래 코드도 똑같은 동작이다. 팀컨벤션만 똑같이 맞추기만하면 됨
+      jest.spyOn(mockUserRepository, 'findOne').mockResolvedValue(user);
+
+      const result = await userService.findOne(1);
+
+      expect(result).toEqual(user);
+      expect(mockUserRepository.findOne).toHaveBeenCalledWith({
+        where: { id: 1 },
+      });
+    });
+
+    it('should throw a NotFoundException if user is not found', async () => {
+      jest.spyOn(mockUserRepository, 'findOne').mockResolvedValue(null);
+
+      expect(userService.findOne(999)).rejects.toThrow(NotFoundException);
+      expect(mockUserRepository.findOne).toHaveBeenCalledWith({
+        where: { id: 999 },
+      });
+    });
+  });
+
   describe('create', () => {});
   describe('update', () => {});
   describe('remove', () => {});
