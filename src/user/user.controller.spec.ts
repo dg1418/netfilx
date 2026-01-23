ee67a64ccac1ee4ba88c0556d8e7c4bfa58c4d72
dg1418 <dg1418@naver.com>
2026-01-23 18:46:42 +0900

    28강 testing : user 컨트롤러 테스트 작성

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,20 +1,144 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { User } from './entity/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+const mockUserService = {
+  findAll: jest.fn(),
+  findOne: jest.fn(),
+  create: jest.fn(),
+  update: jest.fn(),
+  remove: jest.fn(),
+};
 
 describe('UserController', () => {
-  // let controller: UserController;
+  let userController: UserController;
+  let userService: UserService;
 
-  // beforeEach(async () => {
-  //   const module: TestingModule = await Test.createTestingModule({
-  //     controllers: [UserController],
-  //     providers: [UserService],
-  //   }).compile();
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UserController],
+      providers: [
+        {
+          provide: UserService,
+          useValue: mockUserService,
+        },
+      ],
+    }).compile();
 
-  //   controller = module.get<UserController>(UserController);
-  // });
+    userController = module.get<UserController>(UserController);
+    userService = module.get<UserService>(UserService);
+  });
 
   it('should be defined', () => {
-    expect(true).toBeDefined();
+    expect(userController).toBeDefined();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('findAll', () => {
+    it('should return a list of users', async () => {
+      const users = [
+        {
+          id: 1,
+          email: 'test@codefactory.ai',
+        },
+        {
+          id: 2,
+          email: 'jc@codefactory.ai',
+        },
+      ];
+
+      const usersAndCount: [User[], number] = [users as User[], 2];
+
+      jest.spyOn(userService, 'findAll').mockResolvedValue(usersAndCount);
+
+      const result = await userController.findAll();
+
+      expect(result).toEqual(usersAndCount);
+      expect(userService.findAll).toHaveBeenCalled();
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a user By id', async () => {
+      const id = 1;
+      const user = {
+        id: 1,
+        email: 'test@codefactory.ai',
+      };
+
+      jest.spyOn(userService, 'findOne').mockResolvedValue(user as User);
+
+      const result = await userController.findOne(id);
+
+      expect(result).toEqual(user);
+      expect(userService.findOne).toHaveBeenCalledWith(id);
+    });
+  });
+
+  describe('create', () => {
+    it('should return correct value', async () => {
+      const createUserDto: CreateUserDto = {
+        email: 'test@codefactory.ai',
+        password: 'umjunsick',
+      };
+
+      const user = {
+        id: 1,
+        email: 'test@codefactory.ai',
+      };
+
+      jest.spyOn(userService, 'create').mockResolvedValue(user as User);
+
+      const result = await userController.create(createUserDto);
+
+      expect(result).toEqual(user);
+      expect(userService.create).toHaveBeenCalledWith(createUserDto);
+    });
+  });
+
+  describe('update', () => {
+    it('should return the updated user', async () => {
+      const id = 1;
+      const updateUserDto: UpdateUserDto = {
+        email: 'test@codefactory.ai',
+        password: 'umjunsick',
+      };
+
+      const user = {
+        id: 1,
+        email: 'test@codefactory.ai',
+      };
+
+      jest.spyOn(userService, 'update').mockResolvedValue(user as User);
+
+      const result = await userController.update(id, updateUserDto);
+
+      expect(result).toEqual(user);
+      expect(userService.update).toHaveBeenCalledWith(id, updateUserDto);
+    });
+  });
+
+  describe('remove', () => {
+    it('should delete user and return id of deleted user', async () => {
+      const id = 1;
+
+      const user = {
+        id: 1,
+        email: 'test@codefactory.ai',
+      };
+
+      jest.spyOn(userService, 'remove').mockResolvedValue(id);
+
+      const result = await userController.remove(id);
+
+      expect(result).toEqual(id);
+      expect(userService.remove).toHaveBeenCalledWith(id);
+    });
   });
 });
